b941414e86837b63d9cf5a26696e68869f413323
VladGoogle <vguglya@4xxi.com>
2021-11-01 16:12:14 +0300

    Next Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 import { User } from './modules/users/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -18,34 +17,58 @@ import { ModproductorderModule } from './modules/modproductorder/modproductorder
 import { TransactionModule } from './modules/transaction/transaction.module';
 import { StripeModule } from './modules/stripe/stripe.module';
 import { Connection } from 'typeorm';
-import {Card} from "./modules/card/card.entity";
-import {Category} from "./modules/category/category.entity";
-import {Label} from "./modules/label/label.entity";
-import {Modifier} from "./modules/modifier/modifier.entity";
-import {Order} from "./modules/order/order.entity";
-import {Product} from "./modules/product/product.entity";
-import {ProductOrder} from "./modules/productorder/productorder.entity";
-import {ModToProd} from "./modules/modtoprod/modtoprod.entity";
-import {ModToProductToOrder} from "./modules/modproductorder/modproductorder.entity";
-import {Transaction} from "./modules/transaction/transaction.entity";
-
+import { Card } from './modules/card/card.entity';
+import { Category } from './modules/category/category.entity';
+import { Label } from './modules/label/label.entity';
+import { Modifier } from './modules/modifier/modifier.entity';
+import { Order } from './modules/order/order.entity';
+import { Product } from './modules/product/product.entity';
+import { ProductOrder } from './modules/productorder/productorder.entity';
+import { ModToProd } from './modules/modtoprod/modtoprod.entity';
+import { ModToProductToOrder } from './modules/modproductorder/modproductorder.entity';
+import { Transaction } from './modules/transaction/transaction.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(
-      {
-        type: "postgres",
-        port: 5432,
-        username: "db_user",
-        password: "db_password",
-        database: "pg_users",
-        entities: [User, Card, Category, Label, Modifier, Order, Product, ProductOrder, ModToProd, ModToProductToOrder, Transaction],
-        migrationsTableName: "custom_migration_table",
-        migrations: ["migration/*.ts"],
-        cli: {
-          migrationsDir: "migration"
-        }
-      }
-  ), UsersModule, AuthModule, ModifiersModule, LabelModule, CategoryModule, ProductModule, ModtoprodModule, CardModule, OrderModule, ProductorderModule, ModproductorderModule, TransactionModule, StripeModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5432,
+      username: 'db_user',
+      password: 'db_password',
+      database: 'pg_users',
+      entities: [
+        User,
+        Card,
+        Category,
+        Label,
+        Modifier,
+        Order,
+        Product,
+        ProductOrder,
+        ModToProd,
+        ModToProductToOrder,
+        Transaction,
+      ],
+      migrationsTableName: 'custom_migration_table',
+      migrations: ['migration/*.ts'],
+      cli: {
+        migrationsDir: 'migration',
+      },
+    }),
+    UsersModule,
+    AuthModule,
+    ModifiersModule,
+    LabelModule,
+    CategoryModule,
+    ProductModule,
+    ModtoprodModule,
+    CardModule,
+    OrderModule,
+    ProductorderModule,
+    ModproductorderModule,
+    TransactionModule,
+    StripeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,21 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import {User} from "./user.entity";
 
+const result = {
+  firstName: 'Vlad',
+  lastName: 'Google',
+  email: 'email2@gmail.com',
+  phone: 3483389398,
+  type: 'admin',
+};
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let userController: UsersController;
+  let userService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    userController = module.get<UsersController>(UsersController);
+    userService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
+
+    const spy = jest.spyOn(userService, "registerUser").mockResolvedValue(result);
     expect(controller).toBeDefined();
   });
-
-
 });
